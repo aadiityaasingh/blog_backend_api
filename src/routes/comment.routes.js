4ca7bfef95af47471ec2c5152c18ba6e6b6a0670
aadiityaasingh <adityasingh.6488@gmail.com>
2026-01-31 21:56:42 +0530

    all comment route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -5,5 +5,6 @@ const commentController = require("../controllers/comment.Controller.js");
 
 router.post("/:postId" , authMiddleware.protect, commentController.createComment);
 router.delete("/:id", authMiddleware.protect, commentController.deleteComment);
+router.get("/post/:postId", commentController.getCommentsByPost )
 
 module.exports = router;

7d226ca46a35933e85666ec057bd1397fca2439f
aadiityaasingh <adityasingh.6488@gmail.com>
2026-01-31 20:29:33 +0530

    like and unlike routes

--- src/routes/post.Routes.js
+++ src/routes/post.Routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post.Controller.js");
+const likeController = require("../controllers/like.Controller.js");
+const bookMarkController = require("../controllers/bookMark.Controller.js");
 const authMiddleware = require("../middlewares/auth.Middleware.js");
 const roleMiddleware = require("../middlewares/role.middleware.js");
 
@@ -10,5 +12,14 @@ router.post("/", authMiddleware.protect, roleMiddleware.authorizeRoles("admin", 
 router.get("/:id", postController.getSinglePost);
 router.patch("/:id", authMiddleware.protect, roleMiddleware.authorizeRoles("admin", "author"), postController.updatePost);
 router.delete("/:id", authMiddleware.protect, roleMiddleware.authorizeRoles("admin", "author"), postController.deletePost);
+
+router.post("/:id/like" , authMiddleware.protect, likeController.likePost);
+router.delete("/:id/like", authMiddleware.protect, likeController.unlikePost);
+router.get("/:id/likes", likeController.getPostLikes);
+
+router.post("/:id/bookmark", authMiddleware.protect, bookMarkController.savePost);
+router.delete("/:id/bookmark", authMiddleware.protect, bookMarkController.removeBookMark);
+
+
 
 module.exports = router;
